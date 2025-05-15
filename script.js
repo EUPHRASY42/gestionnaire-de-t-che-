dcb13f0098c2318f6f27fbfddfbd810012608a74
EUPHRASY42 <EUPHRASY.NTOLOMEYO@latoile.em-lyon.com>
2025-05-15 10:05:33 +0200

    css

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// let tableautask=[task];
+document.getElementById("boutton").addEventListener("click",function (params) {
+    let task=document.getElementById("input").value;
+    console.log(`${task}`);
+    
+})
+ let tableautask=throw new Error("");
+ 
